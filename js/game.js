c3ca533d4d8746c83c36a4831ab094aedc8d22c1
Benjamin Kloss <107155469+BennieB12@users.noreply.github.com>
2024-10-20 01:05:10 +0200

    added keyboardcontrols

--- js/game.js
+++ js/game.js
@@ -1,5 +1,6 @@
 let canvas;
 let world;
+let keyboard = new Keyboard();
 
 function init(){
     canvas = document.getElementById('canvas');
@@ -9,3 +10,32 @@ function init(){
     
     console.log("my character is",world.character);
 }
+
+document.addEventListener("keydown", (e) => {
+    if (e.keyCode === 38) {
+        keyboard.UP = true;
+    } else if (e.keyCode === 40) {
+        keyboard.DOWN = true;
+    } else if (e.keyCode === 37) {
+      keyboard.LEFT = true;
+    } else if (e.keyCode === 39) {
+      keyboard.RIGHT = true;
+    } else if (e.keyCode === 32) {
+        keyboard.SPACE = true;
+      }
+  });
+
+  document.addEventListener("keyup", (e) => {
+    if (e.keyCode === 38) {
+        keyboard.UP = false;
+    } else if (e.keyCode === 40) {
+        keyboard.DOWN = false;
+    } else if (e.keyCode === 37) {
+        keyboard.LEFT = false;
+    } else if (e.keyCode === 39) {
+      keyboard.RIGHT = false;
+    } else if (e.keyCode === 32) {
+        keyboard.SPACE = false;
+      }
+  });
+
