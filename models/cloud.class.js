c53d7145e3789542ce3f0dfdaf64c2bbfc0f1b8e
Benjamin Kloss <107155469+BennieB12@users.noreply.github.com>
2024-10-22 12:49:18 +0200

    placement of clouds

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -1,12 +1,13 @@
 class Cloud extends MovableObject {
 
-    y = 20;
+    y = 2;
     height = 250;
     width = 500;
     
-    constructor(imagePath, x) {
+    constructor(imagePath, x, y) {
         super().loadImage(imagePath);
         this.x = x + Math.random() * 200;
+        this.y = y + Math.random() * 50;
         this.moveLeft();
     }
 }
